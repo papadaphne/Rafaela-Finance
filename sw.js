c111b16263385cf7ff03d616b07bfefb005f5d06
papadaphne <arinkumanja76@gmail.com>
2025-09-19 11:43:59 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,35 +1,28 @@
-
-const CACHE_NAME = 'rafaela-print-cache-v2';
-const URLS_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './rafaela_logo_mono.png',
-  'https://cdn.tailwindcss.com',
-  'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+const CACHE_NAME = 'rafaela-finance-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      }))
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
     )
   );
 });
